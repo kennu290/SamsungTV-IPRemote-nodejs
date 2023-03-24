723df875d097f101af57561906671b713853daad
KennuByte <73755588+kennu290@users.noreply.github.com>
2023-03-24 20:33:41 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 let samsungTV = require('SamsungTv-IPRemote');
-samsungTV = new samsungTV("HomeControlV2", "192.168.3.61", "8002", "17084333");
+samsungTV = new samsungTV("Samsung TV IP Remote", "192.168.1.1", "8002", "0000000");
 
-//console.log(samsungTV.IP);    // Output: '192.168.3.61'
+//console.log(samsungTV.IP);    // Output: '192.168.1.1'
 //console.log(samsungTV.PORT);  // Output: 8002
-//console.log(samsungTV.TOKEN); // Output: 'abc123'
+//console.log(samsungTV.TOKEN); // Output: '0000000'
 
 console.log(samsungTV.sendKey('Home'))
